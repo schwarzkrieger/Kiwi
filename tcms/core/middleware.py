824eb77feb4db742d2180b084e31cd46c1511c52
Ivajlo Karabojkov <karabojkov@kitbg.com>
2021-01-27 11:57:07 +0200

    Avoid resolve() and make pylint happier

--- tcms/core/middleware.py
+++ tcms/core/middleware.py
@@ -7,7 +7,7 @@ from django.db import DEFAULT_DB_ALIAS, connections
 from django.db.migrations.executor import MigrationExecutor
 from django.db.utils import OperationalError
 from django.http import HttpResponseRedirect
-from django.urls import resolve, reverse
+from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
@@ -15,53 +15,56 @@ from django.utils.translation import gettext_lazy as _
 
 class CheckSettingsMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if resolve(request.path).view_name != "setup":
-            doc_url = "https://kiwitcms.readthedocs.io/en/latest/admin.html#configure-kiwi-s-base-url"
-            try:
-                site = Site.objects.get(pk=settings.SITE_ID)
-            except OperationalError:
-                # Redirect to Setup view
-                return HttpResponseRedirect(reverse("setup"))
+        doc_url = "https://kiwitcms.readthedocs.io/en/latest/admin.html#configure-kiwi-s-base-url"
+        if "setup" in request.path:
+            return None
+        try:
+            site = Site.objects.get(pk=settings.SITE_ID)
+        except OperationalError:
+            # Redirect to Setup view
+            return HttpResponseRedirect(reverse("setup"))
 
-            if site.domain == "127.0.0.1:8000":
-                messages.add_message(
-                    request,
-                    messages.ERROR,
-                    mark_safe(  # nosec:B308:B703
-                        _(
-                            "Base URL is not configured! "
-                            'See <a href="%(doc_url)s">documentation</a> and '
-                            '<a href="%(admin_url)s">change it</a>'
-                        )
-                        % {
-                            "doc_url": doc_url,
-                            "admin_url": reverse("admin:sites_site_change", args=[site.pk]),
-                        }
-                    ),
-                )
+        if site.domain == "127.0.0.1:8000":
+            messages.add_message(
+                request,
+                messages.ERROR,
+                mark_safe(  # nosec:B308:B703
+                    _(
+                        "Base URL is not configured! "
+                        'See <a href="%(doc_url)s">documentation</a> and '
+                        '<a href="%(admin_url)s">change it</a>'
+                    )
+                    % {
+                        "doc_url": doc_url,
+                        "admin_url": reverse("admin:sites_site_change", args=[site.pk]),
+                    }
+                ),
+            )
+        return None
 
 
 class CheckUnappliedMigrationsMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if resolve(request.path).view_name != "setup":
-            doc_url = (
-                "https://kiwitcms.readthedocs.io/en/latest/"
-                "installing_docker.html#initial-configuration-of-running-container"
+        if "setup" in request.path:
+            return
+        doc_url = (
+            "https://kiwitcms.readthedocs.io/en/latest/"
+            "installing_docker.html#initial-configuration-of-running-container"
+        )
+        executor = MigrationExecutor(connections[DEFAULT_DB_ALIAS])
+        plan = executor.migration_plan(executor.loader.graph.leaf_nodes())
+        if plan:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                mark_safe(  # nosec:B308:B703
+                    _(
+                        "You have %(unapplied_migration_count)s unapplied migration(s). "
+                        'See <a href="%(doc_url)s">documentation</a>'
+                    )
+                    % {
+                        "unapplied_migration_count": len(plan),
+                        "doc_url": doc_url,
+                    }
+                ),
             )
-            executor = MigrationExecutor(connections[DEFAULT_DB_ALIAS])
-            plan = executor.migration_plan(executor.loader.graph.leaf_nodes())
-            if plan:
-                messages.add_message(
-                    request,
-                    messages.ERROR,
-                    mark_safe(  # nosec:B308:B703
-                        _(
-                            "You have %(unapplied_migration_count)s unapplied migration(s). "
-                            'See <a href="%(doc_url)s">documentation</a>'
-                        )
-                        % {
-                            "unapplied_migration_count": len(plan),
-                            "doc_url": doc_url,
-                        }
-                    ),
-                )
